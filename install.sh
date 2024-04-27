ca8653dec92ec3a9e7565b5bed4504e33b71ce4d
Massimo Gismondi <omino.gis@gmail.com>
2024-04-27 18:44:59 +0200

    Fix

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ usermod -a -G dialout fablab
 ##################
 #    SOFTWARE    #
 ##################
-apt -y update
-apt -y upgrade
+apt update
+apt upgrade --yes
 apt -y install $(grep -vE "#" list.txt | tr "\n" " ")
 apt -y autoremove
 
@@ -36,8 +36,8 @@ apt -y autoremove
 # URL per scaricare software stampa 3D
 URL_SW_STAMPANTE_3D="https://en.fss.flashforge.com/10000/software/e02d016281d06012ea71a671d1e1fdb7.deb"
 # Installare software FlashPrint
-wget -o /tmp/flashprint.deb "${URL_SW_STAMPANTE_3D}"
-apt install /tmp/flashprint.deb
+wget "${URL_SW_STAMPANTE_3D}"
+apt install ./flashprint.deb
 
 
 
